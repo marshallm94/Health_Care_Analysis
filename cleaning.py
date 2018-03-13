2f354394a66e2311b4d142f58e48651d57580950
Marshall McQuillen <marshallm94@gmail.com>
2018-03-13 11:28:54 -0600

    Change format_excel(): column naming complete

--- cleaning.py
+++ cleaning.py
@@ -4,20 +4,36 @@ import matplotlib.pyplot as plt
 import xlrd
 import re
 
-def clean_excel(filepath):
-    df = pd.read_excel("/Users/marsh/galvanize/dsi/projects/health_capstone/data/medicare_spending_by_county/pa_reimb_county_2003.xls")
+def format_excel(filepath):
+    df = pd.read_excel(filepath)
 
-pattern = "^Unnamed"
-for column in test.columns:
-    if "Unnamed" in column:
-        print(column)
-    else:
-        continue
+    for column in df.columns:
+        if "Unnamed" in column:
+            loc = df.columns.get_loc(column)
+            header = df.columns[loc - 1].replace(" ", "_").lower()
+            new_name = header + "_price_age_sex_race_adj"
+            values = df[column].values
+            df.insert(loc, new_name, values)
+            df.drop(column, axis=1, inplace=True)
+
+    for x, column in enumerate(df.columns):
+        if df[column][0] == "Age, sex & race-adjusted":
+            loc = df.columns.get_loc(column)
+            header = column.replace(" ", "_").lower()
+            new_name = header + "_age_sex_race_adj"
+            values = df[column].values
+            df.insert(loc, new_name, values)
+            df.drop(column, axis=1, inplace=True)
+        elif x in [0,1,2]:
+            new_name = column.lower().replace(" ", "_")
+            values = df[column].values
+            loc = df.columns.get_loc(column)
+            df.insert(loc, new_name, values)
+            df.drop(column, axis=1, inplace=True)
+
+    return df.iloc[1:,:].reset_index(drop=True)
 
 
-
-
-    return df
 
 if __name__ == "__main__":
     # import csv's
@@ -26,4 +42,4 @@ if __name__ == "__main__":
 
     xls = pd.ExcelFile("/Users/marsh/galvanize/dsi/projects/health_capstone/data/medicare_spending_by_county/pa_reimb_county_2003.xls")
 
-    test = clean_excel
+    df = format_excel("/Users/marsh/galvanize/dsi/projects/health_capstone/data/medicare_spending_by_county/pa_reimb_county_2003.xls")
